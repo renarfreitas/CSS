1f9b248c7e5b4eacf24d35e12264d7a31187ed20
Renar Freitas dos Santos <38799859+renarfreitas@users.noreply.github.com>
2020-04-02 13:16:01 -0300

    Commit final
    Este trabalho foi realizado seguindo passo a passo o vídeo: https://www.youtube.com/watch?v=B6a068z8Rkk

--- calendario.js
+++ calendario.js
@@ -1,12 +1,15 @@
-let dt = new Date();
-dt.setDate(1);
-let day = dt.getDay();
-console.log(dt.getDay());
-let endDate = new Date(dt.getFullYear(), dt.getMonth() + 1, 0).getDate();
+let dt = new Date()
 
-let prevDate = new Date(dt.getFullYear(), dt.getMonth(), 0).getDate();
+function RenderDate() {
+dt.setDate(1)
+let day = dt.getDay()
 
-let today = new Date().getDate();
+let endDate = new Date(dt.getFullYear(), dt.getMonth() + 1, 0).getDate()
+
+let prevDate = new Date(dt.getFullYear(), dt.getMonth(), 0).getDate()
+
+let today = new Date()
+console.log(today)
 let months = [
   "January",
   "February",
@@ -20,26 +23,31 @@ let months = [
   "October",
   "Nobermber",
   "December"
-];
+]
 
-document.getElementById("date_str").innerHTML = dt.toDateString();
-document.getElementById("month").innerHTML = months[dt.getMonth()];
+document.getElementById("date_str").innerHTML = dt.toDateString()
+document.getElementById("month").innerHTML = months[dt.getMonth()]
 
-let cells ="";
+let cells =""
 
 for (x = day; x > 0; x--){
   cells+= "<div class='prev_date'>" + (prevDate - x + 1)  + "</div>"
 }
 for (i = 1; i <= endDate; i++){
-  if (i == today){
-    cells+="<div class='today'>" + i + "</div>";
+  if (i == today.getDate() && dt.getMonth()== today.getMonth()){
+    cells+="<div class='today'>" + i + "</div>"
   } else {
-    cells+="<div>" + i + "</div>";
+    cells+="<div>" + i + "</div>"
   }
 }
 
-document.getElementsByClassName("days")[0].innerHTML = cells;
-
+document.getElementsByClassName("days")[0].innerHTML = cells
+}
 function moveDate(para){
-  
+  if (para ==  'prev'){
+    dt.setMonth(dt.getMonth() - 1)
+  } else if(para =='next'){
+    dt.setMonth(dt.getMonth() + 1)
+  }
+  RenderDate()
 }
